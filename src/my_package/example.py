72300cfcd531ade9d1b33c10eb7ffa6499d94e37
Ivelina Momcheva <ivelina.momcheva@gmail.com>
2022-03-25 15:00:31 +0100

    Flake8 changes.

--- src/my_package/example.py
+++ src/my_package/example.py
@@ -1,22 +1,21 @@
 def add_one(number):
-	"""
-	Example of a simple function.
+    """
+    Example of a simple function.
 
-	Parameters
-	----------
-	number: int, float, str
+    Parameters
+    ----------
+    number: int, float, str
 
-	Returns
-	-------
-	out: int, float, str
-	    The input value plus one. Raises TypeError if the input is not
-	    the expected type
+    Returns
+    -------
+    out: int, float, str
+        The input value plus one. Raises TypeError if the input is not
+        the expected type
+    """
 
-	"""
-
-	if isinstance(number, (float,int)):
-		return number + 1
-	elif isinstance(number, (str)):
-		return number + '1'
-	else:
-		raise TypeError('Expecting an int, float or string.')
+    if isinstance(number, (float, int)):
+        return number + 1
+    elif isinstance(number, (str)):
+        return number + '1'
+    else:
+        raise TypeError('Expecting an int, float or string.')
