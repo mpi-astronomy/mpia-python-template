2ef6a1cca3de4a2c14601dc67ce97b4e6bdd34c0
Ivelina Momcheva <ivelina.momcheva@gmail.com>
2022-04-12 12:25:19 +0200

    Change import, simplifies the tests.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,17 +1,16 @@
-import pytest
-import my_package.example
+from my_package.example import add_one
 
 def test_type():
     """
     Test addition with float, int and strung.
     """
-    assert my_package.example.add_one(1) == 2
-    assert my_package.example.add_one(1.1) == 2.1
-    assert my_package.example.add_one('1') == '11'
+    assert add_one(1) == 2
+    assert add_one(1.1) == 2.1
+    assert add_one('1') == '11'
 
 def test_error():
     """
     Test addition with list.
     """
     with pytest.raises(TypeError):
-        my_package.example.add_one([1])
+        add_one([1])
